69a03691a7f2fe6f246c3dd9e951d9dac38f8f09
mchait <miriamchait@gmail.com>
2023-11-23 14:23:22 -0500

    asyn/await

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 let favNumber = 40;
 let url = "http://numbersapi.com";
-// // # 1
+// # 1 with promises
 axios.get(`${url}/${favNumber}?json`)
   .then(res => {
-    console.log(res)
+    console.log(res.data.text)
   })
   .catch(err => console.log("REJECTED!!", err))
 
-// # 2
+// # 1 with async/await
+async function getNumFact() {
+  let fact = await axios.get(`${url}/${favNumber}?json`)
+  console.log(fact.data.text)
+}
+getNumFact()
 
-axios.get(`${url}/3,7,89?json`)
+// 2 with promises
+let favoriteNums = [18, 36, 72, 108]
+axios.get(`${url}/${favoriteNums}?json`)
   .then(res => {
     console.log(res.data)
   })
   .catch(err => console.log("REJECTED!!", err))
 
-// # 3
+// 2 with await/async
+
+async function getMultNumberFacts() {
+  let favNumFacts = await axios.get(`${url}/${favoriteNums}?json`)
+  console.log(favNumFacts.data)
+}
+getMultNumberFacts()
+
+// # 3 with promises
 let fourNumberFacts = [];
 
 for (let i = 1; i < 5; i++) {
   fourNumberFacts.push(
-    axios.get(`http://numbersapi.com/18?json`)
+    axios.get(`${url}/${favNumber}?json`)
   );
 }
 
@@ -28,7 +43,20 @@ Promise.all(fourNumberFacts)
   .then(factsArr => {
     for (res of factsArr) {
       $("body").append(`<p>${res.data.text}</p>`)
-      // console.log(res.data.text)
     }
   })
   .catch(err => console.log(err));
+
+// # 3 with async/await
+async function FourFacts() {
+  for (let i = 1; i < 5; i++) {
+    fourNumberFacts.push(
+      axios.get(`${url}/${favNumber}?json`)
+    );
+  }
+  let facts = await Promise.all(fourNumberFacts)
+  for (let fact of facts) {
+    $("body").append(`<p>${fact.data.text}</p>`)
+  }
+}
+FourFacts()
--- app2.js
+++ app2.js
@@ -1,12 +1,17 @@
 let baseURL = "https://deckofcardsapi.com/api/deck"
 let firstCard = ""
 let deck_id = ""
-// 1
+// 1 with promises
 $.getJSON(`${baseURL}/new/draw/`).then(res => {
     console.log(`${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`);
 });
-
-//2
+// 1 with async
+async function part1() {
+    let res = await $.getJSON(`${baseURL}/new/draw/`)
+    console.log(`${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`);
+}
+part1()
+//2 with promises
 $.getJSON(`${baseURL}/new/draw/`).then(res => {
     firstCard = `${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`
     return $.getJSON(`${baseURL}/${res.deck_id}/draw/?count=1`)
@@ -14,7 +19,15 @@ $.getJSON(`${baseURL}/new/draw/`).then(res => {
 }).then(res => {
     console.log(firstCard, `, ${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`)
 })
-//3
+//2 with async/await
+async function part2() {
+    let res = await $.getJSON(`${baseURL}/new/draw/`)
+    firstCard = `${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`
+    res = await $.getJSON(`${baseURL}/${res.deck_id}/draw/?count=1`)
+    console.log(firstCard, `, ${res.cards[0].value.toLowerCase()} of ${res.cards[0].suit.toLowerCase()}`)
+}
+part2()
+//3 with promises
 let $cardButton = $("#card_button")
 let $card = $("#card")
 $.getJSON(`${baseURL}/new/shuffle`).then(res => {
@@ -33,3 +46,20 @@ $cardButton.click(() => {
         }
     })
 })
+
+//3 with async/await
+async function part3() {
+    let res = await $.getJSON(`${baseURL}/new/shuffle`)
+    deck_id = res.deck_id
+    $cardButton.show()
+    $cardButton.click(async function () {
+        res = await $.getJSON(`${baseURL}/${deck_id}/draw/?count=1`)
+        if (res.remaining > 0) {
+            $card.html(`<img src=${res.cards[0].image}>`)
+        }
+        else {
+            $card.html("")
+            $cardButton.remove()
+        }
+    })
+}
